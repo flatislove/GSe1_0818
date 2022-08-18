207d9c22ec660f35db207f23e66bd3c4530e3c9c
flvtisv <flatislove@gmail.com>
2022-08-19 00:20:23 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int number = 0;
+Console.WriteLine("Введите число");
+try
+{
+    number = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Введите корректное значение");
+    Environment.Exit(0);
+}
+if (number % 2 == 0)
+{
+    Console.Write("Число является четным - ");
+    Console.WriteLine(number);
+}
+else
+{
+    Console.Write("Число не является четным - ");
+    Console.WriteLine(number);
+}
+
