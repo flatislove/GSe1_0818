eae4b0131f0f3457747539cc13a1f03c148d98c8
flvtisv <flatislove@gmail.com>
2022-08-19 00:51:03 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+int number = 0;
+int start = 1;
+Console.WriteLine("Введите число");
+try
+{
+    number = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Некорректное значение");
+    Environment.Exit(0);
+}
+if (number > 0 && number != 1)
+{
+
+    while (start <= number)
+    {
+        if (start % 2 == 0)
+        {
+            Console.Write(start + ",");
+        }
+        start++;
+    }
+    Console.WriteLine();
+}
+else if (number < 0 && number != 1)
+{
+    while (start >= number)
+    {
+        if (start % 2 == 0)
+        {
+            Console.Write(start + "  ");
+        }
+        start--;
+    }
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("Четные числа в диапазоне отсутствуют");
+}
